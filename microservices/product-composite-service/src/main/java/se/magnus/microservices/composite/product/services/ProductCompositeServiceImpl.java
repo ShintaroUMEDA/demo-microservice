2ce63661683d4f67ac75b769c7d6b524ba22ad9e
ShintaroUMEDA <umeda.shintaro.3xk@jp.nssol.nipponsteel.com>
2023-06-13 17:16:30 +0900

    update api test

--- microservices/product-composite-service/src/main/java/se/magnus/microservices/composite/product/services/ProductCompositeServiceImpl.java
+++ microservices/product-composite-service/src/main/java/se/magnus/microservices/composite/product/services/ProductCompositeServiceImpl.java
@@ -81,7 +81,7 @@ public class ProductCompositeServiceImpl implements ProductCompositeService {
   @Override
   public Mono<ProductAggregate> getProduct(int productId, int delay, int faultPercent) {
 
-    LOG.info("Will get composite product info for product.id={}", productId);
+    LOG.info("hoge Will get composite product info for product.id={}", productId);
     return Mono.zip(
       values -> createProductAggregate(
         (SecurityContext) values[0], (Product) values[1], (List<Recommendation>) values[2], (List<Review>) values[3], serviceUtil.getServiceAddress()),
